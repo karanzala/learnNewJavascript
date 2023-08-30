bdddd8439223c026b8fa14136556c7db4064c770
karanzala <zalakaransinh777@gmail.com>
2023-08-30 13:26:45 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const userId = 1234;
+let name = "karan";
+var city = "Ahmedabad";
+age = 26;
+
+/*
+prefer not to use var
+because of issue with the block scope and functions scope
+*/
+
+console.log(userId)
+console.table([userId, name, city, age])
