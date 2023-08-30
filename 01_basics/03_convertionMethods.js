7549ca1bfab22ce3725fa2f9dc7f0792e52b485a
karanzala <zalakaransinh777@gmail.com>
2023-08-30 16:50:06 +0000

    conversions datatypes oprations

---
+++ 01_basics/03_convertionMethods.js
@@ -0,0 +1,32 @@
+// const myAge = "23";
+// const myAge = true;
+// const myAge = null;
+const myAge = undefined ;
+// console.log(myAge, typeof myAge);
+
+const numberAge = Number(myAge);
+// console.log(numberAge, typeof numberAge);
+
+// into Number
+// "23" => 23
+// true => 1
+// null => 0
+// undefined => NuN
+
+let score = null;
+// console.log(score, typeof score);
+
+const inBoolean = Boolean(score);
+// console.log(inBoolean, typeof inBoolean)
+
+//into Boolean
+// 0 => false 23 or any value => true
+// "karan" => true "" => false
+// undefined => false
+// null => false
+
+let marks;
+console.log(marks, typeof marks)
+
+let stringMarks = String(marks)
+console.log(stringMarks, typeof stringMarks)
